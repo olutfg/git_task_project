0ea3bb86702c75a1d45e1c69938af09371a29417
olutfg <olu@tfgconsulting.uk>
2024-03-27 17:00:20 +0000

    Added user input for age

--- helloWorld.py
+++ helloWorld.py
@@ -3,3 +3,6 @@ print("Git is awesome!")
 # request for and print the user's name
 name = input("Enter your name: ")
 print("Your name is: " + name)
+# request for and print the user's age
+age = input("Enter your age: ")
+print("Your age is: " + str(age))
